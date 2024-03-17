2b081658cc5953117a3b1ac3462a57d36963c7f5
kg68k <78926718+kg68k@users.noreply.github.com>
2024-03-17 23:26:01 +0900

    prepare to release gcc1p

---
+++ src/Package.mk
@@ -0,0 +1,59 @@
+# Makefile to create distribution package
+
+CP = cp -p
+RM = rm
+MKDIR = mkdir
+ZIP = zip -9
+STRIP = strip
+U8TOSJ = u8tosj
+
+SRCDIR_MK = ../srcdir.mk
+SRC_DIR = ../src
+-include $(SRCDIR_MK)
+
+T = pkgtmp
+
+GCC_ZIP = gcc130p.zip
+
+FILES = bin/gcc.x bin/gcc_cc1.x bin/gcc_cpp.x lib/libgnu.a \
+	CHANGELOG.txt README.txt libgcc_CHANGELOG.txt libgcc_README.txt
+
+.PHONY: all archive directories
+
+all: directories $(GCC_ZIP)
+
+directories: $(T) $(T)/bin $(T)/lib
+
+$(T) $(T)/bin $(T)/lib:
+	$(MKDIR) $@
+
+$(GCC_ZIP): $(addprefix $(T)/,$(FILES))
+	(cd $(T); $(ZIP) ../$@ $(FILES))
+
+$(T)/bin/gcc.x: gcc.x
+	$(CP) $^ $@
+	$(STRIP) $@
+
+$(T)/bin/gcc_cc1.x: cc1.x
+	$(CP) $^ $@
+	$(STRIP) $@
+
+$(T)/bin/gcc_cpp.x: cpp.x
+	$(CP) $^ $@
+	$(STRIP) $@
+
+$(T)/lib/libgnu.a: ../libgcc/build/libgcc.a
+	$(CP) $^ $@
+
+$(T)/CHANGELOG.txt: $(SRC_DIR)/../CHANGELOG.md
+	$(U8TOSJ) < $^ >! $@
+
+$(T)/README.txt: $(SRC_DIR)/../README.md
+	$(U8TOSJ) < $^ >! $@
+
+$(T)/libgcc_CHANGELOG.txt: $(SRC_DIR)/../libgcc/CHANGELOG.md
+	$(U8TOSJ) < $^ >! $@
+
+$(T)/libgcc_README.txt: $(SRC_DIR)/../libgcc/README.md
+	$(U8TOSJ) < $^ >! $@
+
